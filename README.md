ae0144e09bc7a803a94ff574e22196a90a003a2f
tund7077 <tund7077@co-well.com.vn>
2020-09-27 01:01:38 +0700

    php v7.3, nodejs v12.x

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # docker-php
 Docker for php, composer, linux packages, nodejs, yarn, parcel-bundler and gulp-cli.
+
+`docker pull kevinduy/php`
+
+# 1. Main
+
+- kevinduy/php:2.0 (kevinduy/php:latest)
+- kevinduy/php:1.0
+
+# 2. Docker-compose
+
+- __docker-compose.yml__:
+
+```yaml
+version: "3"
+
+services:
+  workspace:
+    image: kevinduy/php
+    tty: true
+    restart: always
+    volumes:
+      - ./:/var/www/app
+```
+
+- __Running docker compose__:
+
+```sh
+docker-compose up -d
+
+# Linux
+docker-compose exec workspace bash
+
+# Windows
+winpty docker-compose exec workspace bash
+```
+
+# 3. Changelog
+
+### [V2.0]
+- php 7.3
+- nodejs 12.x
+- composer
+- fpm
+- supervisor
+- support. for laravel 8x, 7x, 6x, 5x, ...
+- php config:
+  + display_errors = On
+  +.memory_limit = 512M
+  + post_max_size = 500M
+  + upload_max_filesize = 500M
+
+### [V1.0]
+- php 7.2
+- nodejs 8.x
+- composer
+- fpm
+- supervisor
+- for laravel <= 7.x
+- php config:
+  + display_errors = On
+  +.memory_limit = 512M
+  + post_max_size = 50M
+  + upload_max_filesize = 50M
--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@
 # Environment Laravel original provisioning script
 # https://github.com/dung13890
 
-# Update Package List
+# ============ Update Package List
 apt-get update
-apt-get install -y software-properties-common locales
+apt-get install -y --no-install-recommends apt-utils
+apt-get install -y --no-install-recommends software-properties-common locales
 
 locale-gen en_US.UTF-8
 
@@ -16,70 +17,92 @@ echo "LC_CTYPE=UTF-8" >> /etc/default/locale
 
 export LANG=en_US.UTF-8
 
-# PPA
+# ============ Add PPA php and Install PHP-CLI 7, some PHP extentions
 apt-add-repository ppa:ondrej/php -y
 
-# Install PHP-CLI 7, some PHP extentions
+# Bug: W: --force-yes is deprecated, use one of the options starting with --allow instead.
+# Fix: --allow-downgrades --allow-remove-essential --allow-change-held-packages instead --force-yes
+# Fix Issue: https://github.com/jontewks/puppeteer-heroku-buildpack/pull/30s
 apt-get update
-apt-get install -y --force-yes \
-    php7.2-cli \
-    php7.2-dev \
-    php7.2-common \
-    php7.2-curl \
-    php7.2-json \
-    php7.2-soap \
-    php7.2-xml \
-    php7.2-mbstring \
-    php7.2-mysql \
-    php7.2-pgsql \
-    php7.2-sqlite \
-    php7.2-sqlite3 \
-    php7.2-zip \
-    php7.2-gd \
-    php7.2-xdebug \
-    php7.2-bcmath \
-    php7.2-intl \
-    php7.2-fpm \
-    php-pear \
-    php-memcached \
-    php-redis \
-    php-apcu \
-    libcurl4-openssl-dev \
-    libedit-dev \
-    libssl-dev \
-    libxml2-dev \
-    xz-utils \
-    sqlite3 \
-    libsqlite3-dev \
-    libmcrypt-dev \
-    libreadline-dev \
-    git \
-    curl \
-    vim \
-    nano \
-    tree \
-    zip \
-    unzip \
-    supervisor
+apt-get install -y --no-install-recommends --allow-downgrades --allow-remove-essential --allow-change-held-packages \
+  php7.3 \
+  php7.3-cli \
+  php7.3-fpm \
+  php7.3-dev \
+  php7.3-common \
+  php7.3-soap \
+  php7.3-zip \
+  php7.3-gd \
+  php7.3-bcmath \
+  php7.3-json \
+  php7.3-mbstring \
+  php7.3-curl \
+  php7.3-xml \
+  php7.3-pdo \
+  php7.3-mysql \
+  php7.3-pgsql \
+  php7.3-sqlite \
+  php7.3-sqlite3 \
+  php7.3-xdebug \
+  php7.3-intl \
+  php-pear \
+  php-memcached \
+  php-redis \
+  php-apcu
 
-# Installing mcrypt on PHP 7.2
-printf "\n" | pecl install mcrypt-1.0.1
-bash -c "echo extension=mcrypt.so > /etc/php/7.2/mods-available/mcrypt.ini"
+apt-get install -y --no-install-recommends --allow-downgrades --allow-remove-essential --allow-change-held-packages \
+  gcc \
+  make \
+  autoconf \
+  libc-dev \
+  pkg-config \
+  git \
+  curl \
+  vim \
+  nano \
+  tree \
+  zip \
+  unzip \
+  supervisor
+
+apt-get install -y --no-install-recommends --allow-downgrades --allow-remove-essential --allow-change-held-packages \
+  libcurl4-openssl-dev \
+  libedit-dev \
+  libssl-dev \
+  libxml2-dev \
+  xz-utils \
+  sqlite3 \
+  libsqlite3-dev \
+  libmcrypt-dev \
+  libreadline-dev \
+  libmagic-dev
+
+apt-get update
+
+# Installing mcrypt on PHP 7.3
+# Note:
+# mcrypt-1.0.1: PHP 7.2.0 -> PHP 7.3.0
+# mcrypt-1.0.2: PHP 7.2.0 -> PHP 7.4.0
+# mcrypt-1.0.3: PHP 7.2.0 -> PHP 8.0.0
+printf "\n" | pecl install mcrypt-1.0.3
+bash -c "echo extension=mcrypt.so > /etc/php/7.3/mods-available/mcrypt.ini"
+
+printf "\n" | pecl install Fileinfo
 
 # Remove load xdebug extension
-sed -i 's/^/;/g' /etc/php/7.2/cli/conf.d/20-xdebug.ini
+sed -i 's/^/;/g' /etc/php/7.3/cli/conf.d/20-xdebug.ini
 
-# Set php7.2-fpm
-sed -i "s/listen =.*/listen = 0.0.0.0:9000/" /etc/php/7.2/fpm/pool.d/www.conf
-sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.2/fpm/php.ini
-sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.2/fpm/php.ini
-sed -i "s/post_max_size = .*/post_max_size = 50M/" /etc/php/7.2/fpm/php.ini
-sed -i "s/upload_max_filesize = .*/upload_max_filesize = 50M/" /etc/php/7.2/fpm/php.ini
+# Set php7.3-fpm
+sed -i "s/listen =.*/listen = 0.0.0.0:9000/" /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.3/fpm/php.ini
+sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.3/fpm/php.ini
+sed -i "s/post_max_size = .*/post_max_size = 500M/" /etc/php/7.3/fpm/php.ini
+sed -i "s/upload_max_filesize = .*/upload_max_filesize = 500M/" /etc/php/7.3/fpm/php.ini
 mkdir -p /var/run/php
 mkdir -p /var/log/php-fpm
-touch /var/run/php/php7.2-fpm.sock
+touch /var/run/php/php7.3-fpm.sock
 
-# Install Composer, PHPCS
+# ============ Install Composer, PHPCS
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 composer global require "squizlabs/php_codesniffer=*"
@@ -87,10 +110,10 @@ composer global require "squizlabs/php_codesniffer=*"
 # Create symlink
 ln -s /root/.composer/vendor/bin/phpcs /usr/bin/phpcs
 
-# Install nodejs
-curl -sL https://deb.nodesource.com/setup_8.x | bash -
+# ============ Install nodejs
+curl -sL https://deb.nodesource.com/setup_12.x | bash -
 apt-get install -y nodejs
 npm install -g yarn parcel-bundler gulp-cli
 
-# Clean up
+# ============ Clean up
 apt-get clean && apt-get autoclean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
